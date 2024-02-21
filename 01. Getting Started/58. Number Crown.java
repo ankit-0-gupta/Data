81b4578cc1752b53b2d6e16c8e9a1f695a512d40
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-02-21 17:37:32 +0530

    Number Crown

---
+++ 01. Getting Started/58. Number Crown.java
@@ -0,0 +1,32 @@
+// Number Crown.
+
+// https://www.codingninjas.com/studio/problems/number-crown_6581894
+
+
+public class Solution
+{
+    public static void numberCrown(int n)
+    {
+        int nst = 1;
+        int nsp = 2 * n - 2;
+        for(int row = 1; row <= n; row++)
+        {
+            int val = 1;
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print(val++);
+            }
+            for(int csp = 1; csp <= nsp; csp++)
+            {
+                System.out.print(" ");
+            }
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print(--val);
+            }
+            nst++;
+            nsp -= 2;
+            System.out.println();
+        }
+    }
+}
